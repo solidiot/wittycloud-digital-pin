deebbc27765c3d76dc0904c4b7e2e14bbfb2e168
Vitche Research Team Developer <developer@vitche.com>
2018-05-19 18:58:49 +0300

    1. Digital pin sensor state reading.

---
+++ wittycloud-digital-pin.ino
@@ -0,0 +1,16 @@
+#define SENSOR_PORT 2
+#define GREEN_PORT 12
+
+void setup() {
+  pinMode(SENSOR_PORT, INPUT);
+  pinMode(GREEN_PORT, OUTPUT);
+}
+
+void loop() {
+  int sensorState = digitalRead(SENSOR_PORT);
+  if (sensorState) {
+    digitalWrite(GREEN_PORT, HIGH);
+  } else {
+    digitalWrite(GREEN_PORT, LOW);
+  }
+}
