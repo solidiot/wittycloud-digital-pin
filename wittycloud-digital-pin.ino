44c39481e025aa1e82d3b996c6b86692b1df0993
Vitche Research Team Developer <developer@vitche.com>
2018-05-19 19:59:27 +0300

    1. Controlling the serial port speed.

--- wittycloud-digital-pin.ino
+++ wittycloud-digital-pin.ino
@@ -1,15 +1,25 @@
 #define SENSOR_PORT 2
 #define GREEN_PORT 12
 
+#include <ESP8266WiFi.h>
+
 void setup() {
+
   pinMode(SENSOR_PORT, INPUT);
   pinMode(GREEN_PORT, OUTPUT);
+
+  // Start the serial port
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("ESP8266 digital pin will start");
+  Serial.setDebugOutput(true);
 }
 
 void loop() {
   int sensorState = digitalRead(SENSOR_PORT);
   if (sensorState) {
     digitalWrite(GREEN_PORT, HIGH);
+    Serial.println("Motion detected");
   } else {
     digitalWrite(GREEN_PORT, LOW);
   }
