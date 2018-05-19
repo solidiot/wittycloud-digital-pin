57f2d30534308c2c0fed52d911995f42cfd0354c
Vitche Research Team <developer@vitche.com>
2018-05-19 23:51:25 +0300

    1. Sending server requests only after PIR detection.

--- wittycloud-digital-pin.ino
+++ wittycloud-digital-pin.ino
@@ -43,28 +43,32 @@ void loop() {
       response = "";
     }
 
-    // Connect to the Web server
-    if (client.connect(ONION_REDIS_API_GATEWAY_IP, ONION_REDIS_API_GATEWAY_PORT)) {
-      Serial.println("Connected to Web server");
-      // client.println("POST /SolidIoT/WittyCloudDigitalPin/PIRMotion HTTP/1.1");
-      client.println("POST /test/Echo/echo HTTP/1.0");
-      client.println("Content-Length: 2");
-      client.println();
-      client.println("{}");
-      client.println();
-    } 
+    int sensorState = digitalRead(SENSOR_PORT);
+    if (sensorState) {
+      
+      digitalWrite(GREEN_PORT, HIGH);
+      
+      char* request = "{\"id\":\"36390db26caa4eb28b695fac106a9f53\",\"status\":true}";
+      Serial.println(request);
+  
+      // Connect to the Web server
+      if (client.connect(ONION_REDIS_API_GATEWAY_IP, ONION_REDIS_API_GATEWAY_PORT)) {
+        Serial.println("Connected to Web server");
+        // client.println("POST /SolidIoT/WittyCloudDigitalPin/PIRMotion HTTP/1.1");
+        client.println("POST /test/Echo/echo HTTP/1.0");
+        client.printf("Content-Length: %d", strlen(request));
+        client.println();
+        client.println();
+        client.println(request);
+        client.println();
+      }
+    } else {
+      digitalWrite(GREEN_PORT, LOW);
+    }
   }
 
   while (client.available()) {
     char c = client.read();
     response += c;
   } 
-
-  int sensorState = digitalRead(SENSOR_PORT);
-  if (sensorState) {
-    digitalWrite(GREEN_PORT, HIGH);
-    Serial.println("Motion detected");
-  } else {
-    digitalWrite(GREEN_PORT, LOW);
-  }
 }
